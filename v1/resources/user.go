0f94f5c376141a1d3d63063e18604cd0c38135da
Tim Förster <github@mailserver.1n3t.de>
2016-05-30 20:37:47 +0000

    Add method comments to user resource.
    Signed-off-by: Tim Förster <github@mailserver.1n3t.de>

--- v1/resources/user.go
+++ v1/resources/user.go
@@ -15,172 +15,220 @@
 package resources
 
 import (
-  "github.com/gin-gonic/gin"
-  "github.com/satori/go.uuid"
-  "golang.org/x/crypto/bcrypt"
-  "gopkg.in/redis.v3"
-  "encoding/base64"
+	"encoding/base64"
+	"github.com/gin-gonic/gin"
+	"github.com/satori/go.uuid"
+	"golang.org/x/crypto/bcrypt"
+	"gopkg.in/redis.v3"
 
-  log "github.com/Sirupsen/logrus"
+	log "github.com/Sirupsen/logrus"
 )
 
+//UserResource - Users API endpoint
 type UserResource struct {
-  Redis *redis.Client
-  Engine *gin.RouterGroup
+	Redis  *redis.Client
+	Engine *gin.RouterGroup
 }
 
+//Routes - Users routing definition
 func (u UserResource) Routes() {
-  u.Engine.GET("/users/:uuid", u.Get)
-  u.Engine.POST("/users", u.Create)
-  u.Engine.POST("/users/:uuid/uuid", u.ResetUuid)
+	u.Engine.GET("/users/:uuid", u.Get)
+	u.Engine.POST("/users", u.Create)
+	u.Engine.POST("/users/:uuid/uuid", u.ResetUUID)
 }
 
+/*
+Get - Fetch user by id
+
+	{
+		"user": {
+			"uuid": <UUID>,
+			"username": <Name of user>
+		}
+	}
+*/
 func (u UserResource) Get(c *gin.Context) {
-  user := User{
-    UserResource: &u,
-    Uuid: c.Param("uuid"),
-  }
-  if user.GetUsername() == "" {
-    NotFound("User", c)
-  } else {
-    c.JSON(200, gin.H{
-      "user": map[string]string{
-        "uuid": user.GetUuid(),
-        "username": user.GetUsername(),
-      },
-    })
-  }
+	user := User{
+		UserResource: &u,
+		UUID:         c.Param("uuid"),
+	}
+	if user.GetUsername() == "" {
+		NotFound("User", c)
+	} else {
+		c.JSON(200, gin.H{
+			"user": map[string]string{
+				"uuid":     user.GetUUID(),
+				"username": user.GetUsername(),
+			},
+		})
+	}
 }
 
+/*
+Create - User by username and password
+
+  # curl $API/users -d 'username=moo' -d 'password=swordfish'
+	{
+		"user":
+			"uuid": ...,
+			"username": ...
+		}
+	}
+
+	# curl $API/users -d 'username=moo' -d 'password=swordfish'
+	=> HTTP 405
+	{
+		"message": "User already available"
+	}
+*/
 func (u UserResource) Create(c *gin.Context) {
-  user := base64.StdEncoding.EncodeToString([]byte(c.PostForm("username")))
-  _, err := u.Redis.Get("user::name::"+user).Result()
-  if err != redis.Nil {
-    c.JSON(405, gin.H{
-      "message": "User already available",
-    })
-  } else {
-    new_user := NewUser(c.PostForm("username"),c.PostForm("password"), &u)
-    if new_user.Save() {
-      c.JSON(201, gin.H{
-        "user":  map[string]string{
-          "uuid": new_user.Uuid,
-          "username": new_user.Username,
-        },
-      })
-    } else { 
-      c.JSON(422, gin.H{
-        "message": "Createing new user failed.",
-      })
-    }
-  }
+	user := base64.StdEncoding.EncodeToString([]byte(c.PostForm("username")))
+	_, err := u.Redis.Get("user::name::" + user).Result()
+	if err != redis.Nil {
+		c.JSON(405, gin.H{
+			"message": "User already available",
+		})
+	} else {
+		newuser := NewUser(c.PostForm("username"), c.PostForm("password"), &u)
+		if newuser.Save() {
+			c.JSON(201, gin.H{
+				"user": map[string]string{
+					"uuid":     newuser.UUID,
+					"username": newuser.Username,
+				},
+			})
+		} else {
+			c.JSON(422, gin.H{
+				"message": "Createing new user failed.",
+			})
+		}
+	}
 }
 
-func (u UserResource) ResetUuid(c *gin.Context) {
-  user := User{
-    UserResource: &u,
-    Uuid: c.Param("uuid"),
-  }
-  if user.GetUsername() == "" {
-    NotFound("User", c)
-  } else {
-    c.JSON(200, gin.H{
-      "user": map[string]string{
-        "uuid": user.ResetUuid(),
-        "username": user.GetUsername(),
-      },
-    })
-  }
+/*
+ResetUUID - reset users uuid.
+
+	# curl -X POST $API/users/<uuid>
+	{
+		"user": {
+			"uuid": <UUID>,
+			"username": <Name of user>
+		}
+	}
+*/
+func (u UserResource) ResetUUID(c *gin.Context) {
+	user := User{
+		UserResource: &u,
+		UUID:         c.Param("uuid"),
+	}
+	if user.GetUsername() == "" {
+		NotFound("User", c)
+	} else {
+		c.JSON(200, gin.H{
+			"user": map[string]string{
+				"uuid":     user.ResetUUID(),
+				"username": user.GetUsername(),
+			},
+		})
+	}
 }
 
+// User model
 type User struct {
-  UserResource *UserResource
-  Uuid string
-  Username string
-  Password string
-  PasswordDigest string
+	UserResource   *UserResource
+	UUID           string
+	Username       string
+	Password       string
+	PasswordDigest string
 }
 
+// NewUser returns new user instance
 func NewUser(username string, password string, ur *UserResource) User {
-  new_user := User{
-    UserResource: ur,
-    Username: username,
-    Password: password,
-    Uuid: uuid.NewV4().String(),
-  }
-  v, _ := bcrypt.GenerateFromPassword([]byte(password), bcrypt.MinCost)
-  new_user.PasswordDigest = string(v)
-  return new_user
+	newuser := User{
+		UserResource: ur,
+		Username:     username,
+		Password:     password,
+		UUID:         uuid.NewV4().String(),
+	}
+	v, _ := bcrypt.GenerateFromPassword([]byte(password), bcrypt.MinCost)
+	newuser.PasswordDigest = string(v)
+	return newuser
 }
 
+// Save user by using current redis session to database.
 func (u *User) Save() bool {
-  pipe := u.UserResource.Redis.Pipeline()
-  defer pipe.Close()
-  pipe.Set("user::id::"+u.GetUuid(), u.EncodedUsername(), 0)
-  pipe.Set("user::name::"+u.EncodedUsername(), u.GetUuid(), 0)
-  pipe.Set("user::pass::"+u.EncodedUsername(), u.GetPasswordDigest(), 0)
-  _, err := pipe.Exec()
-  if err != nil {
-    log.Error(err, "Error during User.Save().")
-    return false
-  } else {
-    return true
-  }
+	pipe := u.UserResource.Redis.Pipeline()
+	defer pipe.Close()
+	pipe.Set("user::id::"+u.GetUUID(), u.EncodedUsername(), 0)
+	pipe.Set("user::name::"+u.EncodedUsername(), u.GetUUID(), 0)
+	pipe.Set("user::pass::"+u.EncodedUsername(), u.GetPasswordDigest(), 0)
+	_, err := pipe.Exec()
+	if err != nil {
+		log.Error(err, "Error during User.Save().")
+		return false
+	}
+	return true
 }
 
+// EncodedUsername encodes the username into base64 to prevent
+// to handle each kind of username.
 func (u *User) EncodedUsername() string {
-  return base64.StdEncoding.EncodeToString([]byte(u.GetUsername()))
+	return base64.StdEncoding.EncodeToString([]byte(u.GetUsername()))
 }
 
-func (u *User) GetUuid() string {
-  if u.Uuid == "" {
-    val, err := u.UserResource.Redis.Get("user::name::"+u.EncodedUsername()).Result()
-    if err != nil {
-      log.Error(err, "Fetching Uuid failed")
-    } else {
-      u.Uuid = val
-    }
-  }
-  return u.Uuid
+// GetUUID returns the objects internal UUID or prefetch them from datastore
+func (u *User) GetUUID() string {
+	if u.UUID == "" {
+		val, err := u.UserResource.Redis.Get("user::name::" + u.EncodedUsername()).Result()
+		if err != nil {
+			log.Error(err, "Fetching UUID failed")
+		} else {
+			u.UUID = val
+		}
+	}
+	return u.UUID
 }
 
-func (u *User) ResetUuid() string {
-  id := uuid.NewV4().String()
-  pipe := u.UserResource.Redis.Pipeline()
-  defer pipe.Close()
-  pipe.Set("user::id::"+id, u.EncodedUsername(), 0)
-  pipe.Set("user::name::"+u.EncodedUsername(), id, 0)
-  pipe.Del("user::id::"+u.GetUuid())
-  _, err := pipe.Exec()
-  if err != nil {
-    log.Error(err, "Error on resetting Uuid.")
-  } else {
-    u.Uuid = id
-  }
-  return id
+// ResetUUID sets a new UUID to current user.
+func (u *User) ResetUUID() string {
+	id := uuid.NewV4().String()
+	pipe := u.UserResource.Redis.Pipeline()
+	defer pipe.Close()
+	pipe.Set("user::id::"+id, u.EncodedUsername(), 0)
+	pipe.Set("user::name::"+u.EncodedUsername(), id, 0)
+	pipe.Del("user::id::" + u.GetUUID())
+	_, err := pipe.Exec()
+	if err != nil {
+		log.Error(err, "Error on resetting UUID.")
+	} else {
+		u.UUID = id
+	}
+	return id
 }
 
+// GetUsername returns the objects internal username or prefetch them from datastore
 func (u *User) GetUsername() string {
-  if u.Username == "" {
-    val, err := u.UserResource.Redis.Get("user::id::"+u.Uuid).Result()
-    if err != nil {
-      log.Error(err, "Fetching Username failed")
-    } else {
-      str, _ := base64.StdEncoding.DecodeString(val)
-      u.Username = string(str)
-    }
-  }
-  return u.Username
+	if u.Username == "" {
+		val, err := u.UserResource.Redis.Get("user::id::" + u.UUID).Result()
+		if err != nil {
+			log.Error(err, "Fetching Username failed")
+		} else {
+			str, _ := base64.StdEncoding.DecodeString(val)
+			u.Username = string(str)
+		}
+	}
+	return u.Username
 }
 
+// GetPasswordDigest returns the objects internal passwordDigest or prefetch them from datastore
 func (u *User) GetPasswordDigest() string {
-  if u.PasswordDigest == "" {
-    val, err := u.UserResource.Redis.Get("user::pass::"+u.EncodedUsername()).Result()
-    if err != nil {
-      log.Error(err, "Fetching PasswordDigest failed")
-    } else {
-      u.PasswordDigest = val
-    }
-  }
-  return u.PasswordDigest
+	if u.PasswordDigest == "" {
+		val, err := u.UserResource.Redis.Get("user::pass::" + u.EncodedUsername()).Result()
+		if err != nil {
+			log.Error(err, "Fetching PasswordDigest failed")
+		} else {
+			u.PasswordDigest = val
+		}
+	}
+	return u.PasswordDigest
 }
